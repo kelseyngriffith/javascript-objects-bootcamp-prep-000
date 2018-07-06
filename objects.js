d74bd62a15e4c0d7c3e7b0d865757b8e2a057b99
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 00:59:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist;
 }
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.slowdive;
+  return playlist;
+}
