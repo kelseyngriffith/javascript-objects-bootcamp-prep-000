fefb4b30fce0c23d5f66e12389f5b2c3c5f4d8a6
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 00:56:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 const playlist = {
-  ['Slowdive'] = 'Alison'
-  ['My Bloody Valentine'] = 'Sometimes'
+  Slowdive: 'Alison',
+  My Bloody Valentine: 'Sometimes'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
