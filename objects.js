33136d9d5d8d0f4255dc749f3106adb29d0b9e79
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-07-06 01:04:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist.slowdive;
+  delete playlist.['slowdive'];
+  return playlist
 }
